b690ed7d6f1752c08f644b4f86da10c92430a0f2
Ronald Michaud III <82068985+illogicaldeveloper@users.noreply.github.com>
2021-05-12 11:08:00 -0400

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -7,28 +7,13 @@ float identity[] = {
     0, 0, 1, 0,
     0, 0, 0, 1
 };
-float matAnswer[] = {
-    0, 0, 0, 0,
-    0, 0, 0, 0,
-    0, 0, 0, 0,
-    0, 0, 0, 0
-};
 
 float matClear(float matrix[]) {
     for (int i=0; i<16; i++) {
         matrix[i] = 0;
     }
 }
-float matAdd(float one[], float two[]) {
-    for (int i=0; i<16; i++) {
-        matAnswer[i] = one[i] + two[i];
-    }
-}
-float matSub(float one[], float two[]) {
-    for (int i=0; i<16; i++) {
-        matAnswer[i] = one[i] - two[i];
-    }
-}
+
 float pointMul( float point[], float matrix[] ) {
      for (int i=0; i<4; i++) {
         matPoint[i] = point[i] * matrix[i];
