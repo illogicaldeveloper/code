766265ca4a511554420c14fa3073580e3b0d5432
Ronald Michaud III <82068985+illogicaldeveloper@users.noreply.github.com>
2021-05-13 06:42:44 -0400

    Add files via upload

---
+++ compare.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int compare(char *string1, char *string2 ) {
+    int len = strlen(string1);
+    if (len != strlen(string2)) return 0;
+    for (int i=0; i<(len/2); i++) {
+        if ((string1[i] != string2[i]) 
+            || (string1[len-i] != string2[len-i]))
+                return 0;
+    } return 1 ;
+}
+
+int main() {
+
+    char string1[] = "testing";    
+
+    if (compare( string1, "testing" )) {
+        printf( "They Are Equal" );
+    }
+    
+    printf( "\n" );
+    return 0;
+
+}
